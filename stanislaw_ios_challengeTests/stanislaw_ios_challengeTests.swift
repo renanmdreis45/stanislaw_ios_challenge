2def049008fefc9fbc21b75f28c8a0aa49c7f19e
renanmartins1 <renan.martins@megaleios.com>
2025-06-03 12:15:39 -0300

    Initial Commit

---
+++ stanislaw_ios_challengeTests/stanislaw_ios_challengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  stanislaw_ios_challengeTests.swift
+//  stanislaw_ios_challengeTests
+//
+//  Created by Renan Martins Dantas Reis on 03/06/25.
+//
+
+import Testing
+@testable import stanislaw_ios_challenge
+
+struct stanislaw_ios_challengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
